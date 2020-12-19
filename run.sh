9cc7d795944d47cfcbdbcda5c5fdf6106f1265bd
星野はじめ <wangxufire+github@gmail.com>
2020-12-19 19:56:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ for((i=1;i<=250;i++));
 do   
   echo "run $i times"
   python3 ./scripts/qq_read.py
-  sleep 5m 
   current=`date +%s`
   runtime=`expr $current - $start`
   if (($runtime>21240)); then # 5.9*60*60
     echo "all work is done"
     break
   fi
+  sleep 5m
 done  
